8442b48464d61061a8693715076a5afa8eec1389
spinwintoken <spinwintoken@gmail.com>
2026-06-27 15:13:00 -0400

    Update signin.js

--- public/signin.js
+++ public/signin.js
@@ -1,6 +1,6 @@
 const client = supabase.createClient(
   "https://fchwxnontbfybypezpge.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjaHd4bm9udGJmeWJ5cGV6cGdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1NDAxNTAsImV4cCI6MjA5NjExNjE1MH0.PTs0fUP0aX21t6RtXyVD__yohn1lFynhgplXhbfZeEI"
+  "YOUR_ANON_KEY_HERE"
 );
 
 async function signin() {
@@ -26,5 +26,6 @@ async function signin() {
     window.location.href = "totp-verify.html";
   }
 }
+
 
 
