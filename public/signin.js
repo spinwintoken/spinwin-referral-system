743d22c4236ecb70e016b57d2825500edf2d5b46
spinwintoken <spinwintoken@gmail.com>
2026-06-30 23:07:25 -0400

    Update signin.js

--- public/signin.js
+++ public/signin.js
@@ -1,6 +1,6 @@
 const client = supabase.createClient(
   "https://fchwxnontbfybypezpge.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjaHd4bm9udGJmeWJ5cGV6cGdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1NDAxNTAsImV4cCI6MjA5NjExNjE1MH0.PTs0fUP0aX21t6RtXyVD__yohn1lFynhgplXhbfZeEI"
+  "sb_publishable_1b_GU_eDqYN8UQoLZ5Xg7g_9_LcVasz"
 );
 
 async function signin() {
